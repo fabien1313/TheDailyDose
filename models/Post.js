8d5a7742c6172901b9d877dfd17fdadbcb2138ce
fabien moreno <fabienmoreno1331@yahoo.com>
2023-03-10 14:02:05 -0600

    User Table

--- models/Post.js
+++ models/Post.js
@@ -1,9 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class post extends Model {}
+class Post extends Model {}
 
-post.init (
+Post.init (
     {
         id: {
             type: DataTypes.INTEGER,
@@ -32,4 +32,4 @@ post.init (
 
 
 
-module.exports = post;
+module.exports = Post;
