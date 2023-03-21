6614c7343bd6daf15600e7de99d06ad1d702bd79
fabien moreno <fabienmoreno1331@yahoo.com>
2023-03-21 15:22:05 -0500

    works locally but heroku having issues with delete

--- utils/auth.js
+++ utils/auth.js
@@ -1,10 +1,14 @@
 const withAuth = (req, res, next) => {
   console.log('withAuth', req.session)
     if (!req.session.user_id) {
+      
       console.log('We are redirecting to login from withAuth')
       res.redirect("/login");
+
     } else {
+
       next();
+
     }
   };
   
